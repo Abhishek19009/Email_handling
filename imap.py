8b0e199f1ff9fbcb98c094464d48fdeacdce40a8
Abhishek Kuriyal <64682956+Abhishek19009@users.noreply.github.com>
2021-08-01 15:57:43 +0530

    Update imap.py

--- imap.py
+++ imap.py
@@ -1,8 +1,17 @@
+/* 
+Short script for deleting old emails using IMAP protocol
+Be careful selected emails will be deleted permanently
+If encountered an error, mail me:- abhishek19@iiserb.ac.in 
+*/
+
+
 import imaplib
 
+# Please update imap_user and imap_pass
+
 imap_host = 'imap.gmail.com'
-imap_user = 'abhishek19@iiserb.ac.in'
-imap_pass = 'Deathnote@13'
+imap_user = 'Your email address here'
+imap_pass = 'Your password here'
 
 
 # connect to host using SSL
@@ -17,16 +26,21 @@ status, message_list = imap.search(None, 'ALL')
 
 messages = message_list[0].split(b' ')
 
+# Specify serial number of email from where you want deletion to start
+
+starting_index = input("Enter starting index for deletion: ")
+
 print("SNo\tStatus")
 for message in messages:
 	
-	if int(message) >= 2000:
+	if int(message) >= int(starting_index):
 		try:
 			imap.store(message, "+FLAGS", "\\Deleted")
 			print(f"{int(message)}\tDeleted")
 		except:
 			print(f"{int(message)}\tFailed")
 
+print("\nMails deleted successfully!!!")
 imap.close()
 
 imap.logout()
